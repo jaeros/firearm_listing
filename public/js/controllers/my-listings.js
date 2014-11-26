5de834dd0ac78eda6f5f03677739e76422b22084
Teancum Besendorfer <teancum.besendorfer@gmail.com>
2014-11-26 02:27:35 -0700

    hard coded some data, and added comments to explain what is going on

--- public/js/controllers/my-listings.js
+++ public/js/controllers/my-listings.js
@@ -3,25 +3,35 @@ var myListings = angular.module('MyListingsController', []);
 myListings.controller('myListingsController', function($scope, Listings) {
 	$scope.globalTest = "My Listings Controller Text";
 
+	/* Take a look at listings.js inside of the ../services folder. That is a nice
+	   little factory to give a framework for our resource requests. */
+
+	/* If you are interested in the express routing going on behind the scenes,
+	   take a look at the ../../../routes folder, listings.js file. */
+
 	console.log('listings!');
 
-	// This is the query without a callback or a promise
-	// Most of the time, when you try to set a variable to an angular request
-	// The data doesn't come back fast enough when you try to use that data
-	// Within the controller at some point
+	/* This is the query without a callback or a promise
+	   Most of the time, when you try to set a variable to an angular request
+	   The data doesn't come back fast enough when you try to use that data
+	   Within the controller at some point */
 	var data = Listings.query();
 	console.log(data);
+	/* However, this data can definitely still be useful it you want to use it
+	   in the html somewhere. If you don't need access to the data on the controller
+	   to do something with it, you can just do a call like this. */
 
+
+
+	/* If you do need to access the data to do some processing, then you would do
+	   something like this. The next two queries do basically the same thing.
+	   The $promise is useful when you find yourself doing a lot of nested callback
+	   functions in the example immediately below this comment. With the $promise,
+	   you can make your code a lot cleaner by not using a ton of nested callback
+	   functions. */
 	Listings.query(function(listings) {
 		data = listings;
 		console.log(listings);
-	});
-
-	console.log(data);
-
-	Listings.get({listingId: 0}, function(listing) {
-		data = listing;
-		console.log(listing);
 	});
 
 	console.log(data);
@@ -32,4 +42,18 @@ myListings.controller('myListingsController', function($scope, Listings) {
 	});
 
 	console.log(data);
+
+
+	/* This last one here is an example of a GET that gets one particular listing,
+	   instead of a list of listings. The queries above will get you all of the
+	   listings. You can do $promise chaining on this get just like on the query above. */
+	Listings.get({listingId: 0}, function(listing) {
+		data = listing;
+		console.log(listing);
+	});
+
+	console.log(data);
+
+
+	// More examples to come.
 });
--- routes/listings.js
+++ routes/listings.js
@@ -1,13 +1,67 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var tempDate = new Date("October 13, 2014 11:13:00").toString();
+
+// Hard coded data
+listings = [
+  {
+    id: 0,
+    userId: 0,
+    title: 'My Awesome Mauser!',
+    firearmId: 12345,
+    customGunSpecs: {},
+    description: 'This Post WWII rifle is such a beauty. Take one shot and you\'ll be hooked!',
+    price: 450.99,
+    photos: [
+      {
+        url: 'http://placehold.it/200x200',
+        description: 'Mauser - M48'
+      }
+    ],
+    pageViews: 12,
+    listedOn: tempDate,
+    refreshedOn: tempDate,
+    Promotion: null,
+    isSold: false,
+    isActive: true
+  },
+  {
+    id: 1,
+    userId: 10,
+    title: 'Glock 42',
+    firearmId: 9000,
+    customGunSpecs: {},
+    description: 'The perfect weapon for concealed carry. .380 AUTO really is big enough, trust me.',
+    price: 425.00,
+    photos: [
+    {
+      url: 'http://placehold.it/200x200',
+      description: 'Glock 42'
+    }
+    ],
+    pageViews: 421,
+    listedOn: tempDate,
+    refreshedOn: tempDate,
+    Promotion: null,
+    isSold: false,
+    isActive: true
+  }
+];
+
+/* GET all listings */
 router.get('/', function(req, res) {
-  res.json([{message: 'listings resources'}]);
+  res.json(listings);
 });
 
+/* GET a listing based on the listing's id */
 router.get('/:listingId', function(req, res) {
-  res.json({id: 0, message: 'listing resource'});
+  listingId = req.params.listingId;
+
+  listings.forEach(function (listing) {
+    if(listing.id == listingId)
+      res.json(listing);
+  });
 });
 
 module.exports = router;
