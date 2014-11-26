63cc3fff74c038120ea0c2d70ea00276ff3bfab4
Teancum Besendorfer <teancum.besendorfer@gmail.com>
2014-11-26 02:14:43 -0700

    examples on how to do simple requests in angular

--- public/js/controllers/my-listings.js
+++ public/js/controllers/my-listings.js
@@ -5,11 +5,31 @@ myListings.controller('myListingsController', function($scope, Listings) {
 
 	console.log('listings!');
 
+	// This is the query without a callback or a promise
+	// Most of the time, when you try to set a variable to an angular request
+	// The data doesn't come back fast enough when you try to use that data
+	// Within the controller at some point
+	var data = Listings.query();
+	console.log(data);
+
 	Listings.query(function(listings) {
+		data = listings;
 		console.log(listings);
 	});
 
+	console.log(data);
+
 	Listings.get({listingId: 0}, function(listing) {
+		data = listing;
 		console.log(listing);
 	});
+
+	console.log(data);
+
+	Listings.query().$promise.then(function(listings) {
+		data = listings;
+		console.log(listings);
+	});
+
+	console.log(data);
 });
