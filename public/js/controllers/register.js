a0b5422812d7702176f38088a923b9c2f35af93c
Jeff Stoddard <jeffsto@outlook.com>
2014-11-29 21:57:23 -0700

    Basic angular code for testing create user

--- public/js/controllers/register.js
+++ public/js/controllers/register.js
@@ -1,5 +1,22 @@
 var register = angular.module('RegisterController', []);
 
-register.controller('registerController', function($scope) {
+register.controller('registerController', function($scope, $http) {
+	console.log("In register controller!");
+
 	$scope.globalTest = "Register Controller Text";
+
+	$scope.user = {};
+
+	$scope.registerUser = function() {
+		console.log("Registering: ", $scope.user);
+
+		// Make request
+		$http.post('users/', $scope.user).
+			success(function(data, status, headers, config) {
+				console.log("Got data: ", data);
+			}).
+			error(function(data, status, headers, config) {
+				console.log("Received creation error: ", data);
+			});
+	};
 });
