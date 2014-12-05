9367cd6b76c5b4efc91e10bb6379a6151d0771f8
Jillian <user@null-e4ce8f47807e.app.byu.edu>
2014-12-04 20:49:23 -0700

    popular listings but with an error

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,25 +3,9 @@ var index = angular.module('IndexController', []);
 index.controller('indexController', function($scope, Listings) {
 	$scope.globalTest = "Index Controller Text";
 
-	var url = window.location.href;
-	var listingId = url.substring(url.lastIndexOf('/')+1);
-	
-	//Get main listing
-	Listings.get({listingId: listingId}, function(listing){
-		listing.pageViews += 1;
-		$scope.listing = listing;
-		$scope.currentPhoto = $scope.listing.photos[0];
-		console.log(listing);
-		listing.$update({listingId: listing._id});
-	});
-
-	$scope.showPhoto = function(photo){
-		$scope.currentPhoto = photo;
-	};
-
 	//Get related listings
 	Listings.query(function(listings) {
-		$scope.relatedListings = listings;
+		$scope.listings = listings;
+		console.log(listings);
 	});
-
 });
