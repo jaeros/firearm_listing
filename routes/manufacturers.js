df77be59506583c0a7612798ccc8789d01da23e9
Jeff Stoddard <jeffsto@outlook.com>
2014-12-08 15:46:56 -0700

    Modified manufacturers collection to only return names and id's

--- routes/manufacturers.js
+++ routes/manufacturers.js
@@ -5,18 +5,38 @@ var config = require('config');
 
 var Manufacturer = model.Manufacturer;
 
-/* GET all manufacturer names */
+/* GET all manufacturer names and id's */
 router.get('/', function(req, res) {
-  Manufacturer.find({}, function(err, docs) {
-  	if(err)
-  	{
-  		res.status(500).send('Could not retrieve manufacturers');
-  	}
-  	else
-  	{
-  		res.status(200).send(docs);
-  	}
-  });
+  	Manufacturer.aggregate([
+  		{$project: {'name': 1}}
+  	], function(err, result) {
+  		if(err)
+		{
+			res.status(500).send('Could not retrieve manufacturers');
+		}
+		else
+		{
+			res.status(200).send(result);
+		}
+  	});
+});
+
+router.get('/:manufacturerId', function(req, res) {
+	Manufacturer.find({'_id': req.params.manufacturerId}, function(err, doc) {
+		if(err)
+		{
+			res.status(500).send('Could not retrieve manufacturer');
+		}
+		else if(!doc)
+		{
+			res.status(404).send('Not found');
+		}
+		else
+		{
+			res.status(200).send(doc);
+		}
+
+	});
 });
 
 module.exports = router;
