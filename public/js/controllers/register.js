9c1dd5fcd9200d4bf115603a5e487972dec8b36f
Jeff Stoddard <jeffsto@outlook.com>
2014-12-09 18:46:27 -0700

    Basic error handling in register page

--- public/js/controllers/register.js
+++ public/js/controllers/register.js
@@ -3,8 +3,7 @@ var register = angular.module('RegisterController', []);
 register.controller('registerController', function($scope, $http, $location) {
 	console.log("In register controller!");
 
-	$scope.globalTest = "Register Controller Text";
-
+	$scope.registerError = false;
 	$scope.user = {};
 
 	$scope.registerUser = function() {
@@ -17,6 +16,7 @@ register.controller('registerController', function($scope, $http, $location) {
 				if(!data)
 					return;
 
+				$scope.registerError = false;
 				$scope.setLoginData(data);
 
 				alert("Your account was registered successfully!");
@@ -25,6 +25,7 @@ register.controller('registerController', function($scope, $http, $location) {
 			}).
 			error(function(data, status, headers, config) {
 				console.log("Received creation error: ", data);
+				$scope.registerError = "Couldn't register a new account at this time. Please try again later.";
 			});
 	};
 });
