a543089d6b0303f59b3886293908076dbcc53fa8
Jillian <jpkoontz@gmail.com>
2014-12-11 17:26:58 -0700

    search button on search page

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -13,6 +13,29 @@ index.controller('indexController', function($scope, Listings, Manufacturers) {
 		console.log(manufacturers);
 	});
 
+    $scope.registerUser = function() {
+		console.log("Registering: ", $scope.user);
+
+		// Make request
+		$http.post('users/', $scope.user).
+			success(function(data, status, headers, config) {
+				console.log("Got data: ", data);
+				if(!data)
+					return;
+
+				$scope.registerError = false;
+				$scope.setLoginData(data);
+
+				alert("Your account was registered successfully!");
+
+				$location.path('/');
+			}).
+			error(function(data, status, headers, config) {
+				console.log("Received creation error: ", data);
+				$scope.registerError = "Couldn't register a new account at this time. Please try again later.";
+			});
+	};
+
 	//var myData = [{id: manufacturerId, label: {{manufacturers_value}} }];
 	//$(".myDropdownCheckbox").dropdownCheckbox({
 	//  data: myData,
